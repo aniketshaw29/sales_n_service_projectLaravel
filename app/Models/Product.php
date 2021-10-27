37fc1faeaf7bcdde9c77b7fa16ac7c36e7ec0bd9
aniketshaw29 <aniketshawcoding@gmail.com>
2021-10-27 22:27:16 +0530

    again 27th oct 2021

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function unit()
+    {
+        return $this->belongsTo('App\Models\Unit','unit_id');
+    }
 }
